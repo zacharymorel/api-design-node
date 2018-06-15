c379b783d350916a57a789c058a872cc0d5cd900
Zachary Morel <morelz133@gmail.com>
2018-06-15 11:04:26 -0400

    refresher on Node/ Mongo/ Mongoose

--- server.js
+++ server.js
@@ -2,4 +2,22 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+const express = require('express')
+const app = express()
+
+let jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html', function(err) {
+    if(err)
+      res.status(500).send(`Err on get '/': ${err}`)
+  })
+})
+
+app.get('/data', function(req, res) {
+  res.json(jsonData)
+})
+
+let port = 3000
+app.listen(port, console.log(`Listening on http://localhost:${port}.`))
+ 
