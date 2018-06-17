c95f6671040bbad17fe69f283e7299bba177f6c4
Zachary Morel <morelz133@gmail.com>
2018-06-16 21:03:46 -0400

    Connecting to Mongo DB

--- dbTester.js
+++ dbTester.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/TODOS')
 // connect to a database so the below will work.
 // what's happening is that we're createing a new schema
 // and making a todos collection and a Todo model.
 // We then create new Todo.
 // So in the mongo shell in the terminal,
-// connect to your database and uery it there
+// connect to your database and query it there
 // and see if you see the todo below log in the terminal
 // run node db.Tester to execute this file
 // MAKE SURE MONGOD is RUNNING
@@ -22,3 +23,4 @@ Todo.create({
 }).then(function(err, todo) {
   console.log(err, todo);
 });
+
